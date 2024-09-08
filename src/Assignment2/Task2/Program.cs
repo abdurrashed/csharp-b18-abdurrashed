6300e571f401f5385c2d6dc966a225d89e002ffe
rashed <rashedroni3@gmail.com>
2024-09-08 11:28:44 +0600

    Task2 completed

---
+++ src/Assignment2/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string string1 = Console.ReadLine();
+string string2 = Console.ReadLine();
+
+
+if (string2.Length > string1.Length)
+{
+    string s = string1;
+    string1 = string2;
+    string2 = s;
+
+}
+
+string2 = string2.PadLeft(string1.Length, '0');
+int length = string1.Length-1;
+int carry = 0;
+string ans ="";
+while (length >=0)
+{
+    int str1cha = string1[length] - '0';
+    int str2cha = string2[length] - '0';
+    int sum = str1cha + str2cha + carry;
+    carry = sum / 10;
+    ans = (sum % 10) + ans;
+
+    length--;
+
+}
+if (carry > 0)
+{
+    ans = carry + ans;
+}
+
+Console.WriteLine(ans);
+
