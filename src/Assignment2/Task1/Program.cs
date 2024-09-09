78e7adbd478a21c1802422d67ff3691df17b5fbc
rashed <rashedroni3@gmail.com>
2024-09-09 19:19:47 +0600

    Task 1 completed

--- src/Assignment2/Task1/Program.cs
+++ src/Assignment2/Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+string str = Console.ReadLine();
+string[] stringarr = str.Split(' ');
+
+for (int j= 0; j < stringarr.Length; j++)
+{
+    stringarr[j] = stringarr[j].Trim();
+}
+
+int numerator = int.Parse(stringarr[0]);
+int denominator = int.Parse(stringarr[1]);
+int length = int.Parse(stringarr[2]);
+
+int res = numerator / denominator;
+int reminder = numerator % denominator;
+int i = 0;
+string ans = res + ".";
+while (i < length)
+{
+    reminder = reminder * 10;
+    res = reminder / denominator;
+    reminder = reminder % denominator;
+
+    ans = ans + res;
+
+    i++;
+}
+
+Console.WriteLine(ans);
