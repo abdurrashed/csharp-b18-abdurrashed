f050c6b56ff6243a4d8f4688e386c8826be0bbec
rashed <rashedroni3@gmail.com>
2024-11-14 13:16:05 +0600

    Assignment-4 Completed

--- src/Assignment4/Assignment4/Program.cs
+++ src/Assignment4/Assignment4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Assignment4;
+
+string filePath = "Data.txt";
+
+string[] lines = File.ReadAllLines(filePath);
+
+
+using TrainingDbContext trainingDbContext = new TrainingDbContext();
+
+
+
+   foreach (string line in lines)
+   {
+
+    string[] parts = line.Split(' ');
+     
+
+    string name = parts[0];
+    int age = int.Parse(parts[1]);
+    double cgpa = double.Parse(parts[2]);
+
+    Student student = new Student();
+    student.Name = name;
+    student.Age = age;
+    student.CGPA = cgpa;
+
+    trainingDbContext.Students.Add(student);
+
+   }
+
+trainingDbContext.SaveChanges();
+
+
+
---
+++ src/Assignment4/Assignment4/TrainingDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class TrainingDbContext : DbContext
+    {
+        private readonly string _connectionstring = "Data Source=.\\SQLEXPRESS;Initial Catalog=CSharpB18;User ID=csharpb18;Password=123456;TrustServerCertificate=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionstring);
+
+            }
+
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Student> Students{ get; set;}
+       
+
+
+    }
+}
+
+
+
